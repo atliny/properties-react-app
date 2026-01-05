9e742990c5e9f7da927e4987b05e8d8ecfaede3c
Anudhi <anudhi@192.168.1.11>
2026-01-05 12:30:33 +0530

    error fix

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/property-react-app/",
   test: {
     globals: true,
     environment: "happy-dom",
